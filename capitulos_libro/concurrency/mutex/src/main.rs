dc30d42704de1d4be42774a4cd976b256d5db143
Alexis Herrera <alexysaguilar@gmail.com>
2022-09-11 12:23:48 -0300

    contador multiples threads

---
+++ capitulos_libro/concurrency/mutex/src/main.rs
@@ -0,0 +1,39 @@
+use std::{sync::{Mutex, Arc}, vec, thread};
+
+fn main() {
+    // example1();
+    example2();
+}
+
+#[allow(dead_code)]
+fn example1() {
+    let m = Mutex::new(5);
+    
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+
+    println!("m = {:?}", m);
+}
+
+#[allow(dead_code)]
+fn example2() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result {}", *counter.lock().unwrap());
+}
